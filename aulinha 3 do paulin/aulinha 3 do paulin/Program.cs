0bf5faf6cfe9677fa2598a2e8228d667e0fab17f
Pedro Henrique Gaspar <mortadela22112005@gmail.com>
2023-08-14 09:11:40 -0300

    MEDIA DE 3 NUM

---
+++ aulinha 3 do paulin/aulinha 3 do paulin/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aulinha_3_do_paulin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double a, b, c, resultado;
+
+            Console.Write("Digite um numero :");
+            a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.Write("Digite um numero :");
+
+            c = int.Parse(Console.ReadLine());
+
+            resultado = Math.Pow(a, c);
+
+           Console.Write("O Resultado = " + resultado );
+
+            Console.WriteLine();
+
+            Console.Write("Digite outro numero: ");
+
+            b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            resultado = Math.Sqrt(b);
+
+            Console.WriteLine("A Raiz quadrada é " + resultado);
+            
+
+
+            
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();  
+        }
+    }
+}
---
+++ media de 3 numeros/media de 3 numeros/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace media_de_3_numeros
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, divisao, soma;
+
+
+            Console.WriteLine("Digite um numero:");
+            
+                a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.WriteLine("Digite um numero:");
+
+                b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.WriteLine("Digite um numero:");
+
+                c = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+                soma = a + b + c;
+
+            divisao = soma / 3;
+
+                Console.WriteLine();
+
+            Console.WriteLine("A media é:" +  divisao);
+
+            Console.ReadKey();
+
+
+
+
+
+
+        }
+    }
+}
